8b51c193980e41f9ce424d85d5cb0f1e8151ef19
ev.myshko <ev.myshko@tensor.ru>
2020-04-15 15:04:35 +0300

    Добавлен модультестирования api

---
+++ tests/test_api.py
@@ -0,0 +1,14 @@
+"""
+    Модуль содержит юнит-тесты API методов модуля image_comparsion
+
+    author: https://github.com/egor43
+"""
+
+import unittest
+
+
+class TestApiMethods(unittest.TestCase):
+    """
+        Набор тестовых кейсов для тестирования API модуля
+    """
+    pass
