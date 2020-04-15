a5877f73625bbf3db4dfc9cb860fcd46a7ffd545
ev.myshko <ev.myshko@tensor.ru>
2020-04-15 18:16:16 +0300

    Добавлены тесты для метода получения изображения по url

--- tests/test_image_opener.py
+++ tests/test_image_opener.py
@@ -14,33 +14,69 @@ class TestImageOpener(unittest.TestCase):
     """
         Набор тестовых кейсов для тестирования получения изображений
     """
-    def test_get_image_empty_raise(self):
+
+    def setUp(self):
+        """
+            Подготовка необходимых данных
+        """
+        self.img_path = "./tests/files/1.png"
+        self.img_url = "https://pythonworld.ru/m/img/python-3.png"
+
+    def test_get_img_empty_raise(self):
         """
             Тестирование получения исключения при передаче пустого байтового потока
         """
         empty_byte_stream = io.BytesIO(b'')
         self.assertRaises(PIL.UnidentifiedImageError, image_opener.get_img, empty_byte_stream)
 
-    def test_get_image(self):
+    def test_get_img(self):
         """
             Тестирование получения изображения
         """
-        with open("./tests/files/1.png", "rb") as img_byte_stream:
+        with open(self.img_path, "rb") as img_byte_stream:
             img = image_opener.get_img(img_byte_stream)
             self.assertTrue(img.getdata())
     
-    def test_get_image_gray_scale(self):
+    def test_get_img_gray_scale(self):
         """
             Тестирование получения изображения в оттенках серого
         """
-        with open("./tests/files/1.png", "rb") as img_byte_stream:
+        with open(self.img_path, "rb") as img_byte_stream:
             img = image_opener.get_img(img_byte_stream, is_gray_scale=True)
             self.assertEqual(img.mode, "L")
     
-    def test_get_image_rgb(self):
+    def test_get_img_not_gray_scale(self):
         """
-            Тестирование получения изображения в rgb
+            Тестирование получения изображения в цветном режиме
         """
-        with open("./tests/files/1.png", "rb") as img_byte_stream:
+        with open(self.img_path, "rb") as img_byte_stream:
             img = image_opener.get_img(img_byte_stream)
-            self.assertEqual(img.mode, "RGB")
+            self.assertNotEqual(img.mode, "L")
+
+    def test_get_img_from_url_empty_raise(self):
+        """
+            Тестирование получения исключения при получении некорректного изображения по url
+        """
+        image_url = "https://ya.ru"
+        self.assertRaises(PIL.UnidentifiedImageError, image_opener.get_img_from_url, image_url)
+    
+    def test_get_img_from_url(self):
+        """
+            Тестирование получения изображения по url
+        """
+        img = image_opener.get_img_from_url(self.img_url)
+        self.assertTrue(img.getdata())
+
+    def test_get_img_from_url_gray_scale(self):
+        """
+            Тестирование получения изображения по url в оттенках серого
+        """
+        img = image_opener.get_img_from_url(self.img_url, is_gray_scale=True)
+        self.assertEqual(img.mode, "L")
+        
+    def test_get_img_from_url_not_gray_scale(self):
+        """
+            Тестирование получения изображения по url в цветном режиме
+        """
+        img = image_opener.get_img_from_url(self.img_url)
+        self.assertNotEqual(img.mode, "L")
