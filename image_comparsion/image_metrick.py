176b8da3488b2d24dc567412e2d24a0f513a0bc9
ev.myshko <ev.myshko@tensor.ru>
2020-04-16 17:24:59 +0300

    Добавлено вычисление основных метрик изображения

---
+++ image_comparsion/image_metrick.py
@@ -0,0 +1,44 @@
+"""
+    Модуль предоставляет инструменты получения метрик изображений
+
+    author: https://github.com/egor43
+"""
+
+import imagehash
+import skimage
+
+
+def average_hash(img, hash_size=128):
+    """
+        Вычисление average hash.
+        Params:
+            img - изображение
+            hash_size - размер хеша
+        Return:
+            ImageHash - объект представляющий хеш изображения
+    """
+    return imagehash.average_hash(img, hash_size)
+
+
+def perceptual_hash(img, hash_size=128):
+    """
+        Вычисление perceptual hash.
+        Params:
+            img - изображение
+            hash_size - размер хеша
+        Return:
+            ImageHash - объект представляющий хеш изображения
+    """
+    return imagehash.phash(img, hash_size)
+
+
+def wavelet_hash(img, hash_size=128):
+    """
+        Вычисление wavelet hash.
+        Params:
+            img - изображение
+            hash_size - размер хеша
+        Return:
+            ImageHash - объект представляющий хеш изображения
+    """
+    return imagehash.whash(img, hash_size)
