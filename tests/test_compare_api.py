f09e3e54498d89feb8212cb9a3fe90ce538f5e77
ev.myshko <ev.myshko@tensor.ru>
2020-04-17 17:51:21 +0300

    Добавлено тестирование для функции определения процента совпадения точек orb дескрипторов

--- tests/test_compare_api.py
+++ tests/test_compare_api.py
@@ -25,6 +25,10 @@ class TestCompareApi(unittest.TestCase):
         self.img2_path = "./tests/files/2.png"
         with open(self.img2_path, "rb") as image_bs:
             self.img2 = image_opener.get_img(image_bs, is_gray_scale=True)
+        
+        self.img3_path = "./tests/files/3.png"
+        with open(self.img3_path, "rb") as image_bs:
+            self.img3 = image_opener.get_img(image_bs, is_gray_scale=True)
     
     def test_hash_match_rates(self):
         """
@@ -57,3 +61,17 @@ class TestCompareApi(unittest.TestCase):
         self.assertTrue("avg" in match_rates)
         self.assertTrue("per" in match_rates)
         self.assertTrue("wav" in match_rates)
+
+    def test_orb_match_rate(self):
+        """
+            Тестирование получения процента совпадения изображений по дескрипторам ORB детектора
+        """
+        match_percent = compare_api.orb_match_rate(self.img2, self.img3)
+        self.assertTrue(match_percent >= 0 and match_percent <= 100)
+
+    def test_orb_match_rate_for_equals_img(self):
+        """
+            Тестирование получения близкого к 100% совпадения одинаковых изображений по дескрипторам ORB детектора
+        """
+        match_percent = compare_api.orb_match_rate(self.img2, self.img2)
+        self.assertTrue(match_percent >= 97 and match_percent <= 100)
