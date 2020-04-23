312f36cf2fa44f69d7edf46e9a0278e77c920d2d
ev.myshko <ev.myshko@tensor.ru>
2020-04-23 11:28:05 +0300

    Изменено описание пакета

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='PyImageComparsion',
     version=image_comparsion.__version__,
     packages=find_packages(),
-    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description='Image comparison package based on imagehash and skimage',
     include_package_data=True,
     install_requires=[line.rstrip() for line in open("requirements.txt", "r").readlines()],
     test_suite='tests'
